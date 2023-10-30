4a31d184859005b6f8d0e70ec49c2020e10a6221
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-30 17:02:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let para = document.getElementById("timer");
+function updateTime(){
+	let now = new Date();
+	let date = now.toLocaleDateString();
+	let time = now.toLocaleTimeString();
+	para.innerText = `${date}, ${time}`;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
